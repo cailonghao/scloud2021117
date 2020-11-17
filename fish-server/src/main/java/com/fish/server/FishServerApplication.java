19bc89e19b8fc11e77fa36a0623cd05fbf274093
longhaocai <cailonghao1234@foxmail.com>
2020-11-17 18:07:42 +0800

    no message

---
+++ fish-server/src/main/java/com/fish/server/FishServerApplication.java
@@ -0,0 +1,13 @@
+package com.fish.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FishServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FishServerApplication.class, args);
+    }
+
+}
---
+++ fish-server/src/test/java/com/fish/server/FishServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fish.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FishServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
