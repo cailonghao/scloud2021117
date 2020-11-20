a3979c09f7ebb9ba07036d028f2438f8a25a3688
longhaocai <cailonghao1234@foxmail.com>
2020-11-20 11:23:24 +0800

    no message

--- stu-jenkins/src/main/java/com/jenkins/stu/controller/IndexController.java
+++ stu-jenkins/src/main/java/com/jenkins/stu/controller/IndexController.java
@@ -1,6 +1,19 @@
-package com.jenkins.stu.controller;/**
+package com.jenkins.stu.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
  * @Description TODO
  * @Date 2020/11/20 10:31
  * @Created by cai
- */public class IndexController {
+ */
+@RestController
+@RequestMapping("/index")
+public class IndexController {
+
+    @RequestMapping("/index")
+    public String   index() {
+        return "index";
+    }
 }
