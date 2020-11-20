8a7c282fec5286e4eaedb2ba04c81da97b47f9ef
longhaocai <cailonghao1234@foxmail.com>
2020-11-20 11:23:11 +0800

    no message

---
+++ stu-jenkins/src/main/java/com/jenkins/stu/controller/IndexController.java
@@ -0,0 +1,6 @@
+package com.jenkins.stu.controller;/**
+ * @Description TODO
+ * @Date 2020/11/20 10:31
+ * @Created by cai
+ */public class IndexController {
+}
---
+++ stu-jenkins/src/test/java/com/jenkins/stu/StuJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jenkins.stu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StuJenkinsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
